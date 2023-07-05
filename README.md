e7d6d33119c011b04609d77d275c3c920bc405fb
pchala <pchala@gmail.com>
2023-07-05 08:56:23 +0200

    release v0.1

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # PicoDoro
-Pomodoro timer on RP2040-LCD-0.96
+
+[Pomodoro](https://en.wikipedia.org/wiki/Pomodoro_Technique) timer on [RP2040-LCD-0.96](https://www.waveshare.com/wiki/RP2040-LCD-0.96)
+
+Code is done for [Micropython v1.20](https://docs.micropython.org/en/v1.20.0/rp2/quickref.html)
+
+![Working timer view](doc/main.jpg)
+
+## User manual
+
+- Use `Reset` button to start timer
+- Time is hardcoded to 25min / 5min in [main.py](src/main.py)
---
+++ src/main.py
@@ -0,0 +1,53 @@
+import lcd as l
+import time
+import machine
+
+
+def drawGauge(fore, back, cur, total):
+    border = 5
+    lcd.fill(back)
+    w = lcd.width-border*2
+    till = w * cur // total
+    lcd.rect(border, border, till, lcd.height-border*2, l.BLACK, True)
+    lcd.rect(border + till, border, w - till, lcd.height-border*2, fore, True)
+    lcd.display()
+
+
+tick = False
+
+
+def timeHandler(t):
+    global tick
+    tick = True
+
+
+def drawTimer(sec, fore, back):
+    global tick
+    tim = machine.Timer(period=1000, mode=machine.Timer.PERIODIC, callback=timeHandler)
+    for s in range(sec):
+        drawGauge(fore, back, s, sec)
+        while not tick:
+            time.sleep(0.1)
+        tick = False
+    tim.deinit()
+    tick = False
+
+
+# check if the device woke from a deep sleep
+if machine.reset_cause() != machine.PWRON_RESET:
+    machine.deepsleep(60000)
+
+lcd = l.LCD_0inch96()
+
+drawTimer(25*60, l.RED, l.BLUE)
+drawTimer(5*60, l.GREEN, l.BLUE)
+
+lcd.fill(l.BLUE)
+lcd.display()
+
+# LCD power off
+lcd.backlight(0)
+lcd.write_cmd(0x10)  
+
+# MCU power off
+machine.deepsleep(60000)
